a5792fa313a831bef2b8a8c27f2496b379640e09
quang3103 <trannhutquang.bi@gmail.com>
2021-11-13 18:45:28 +0700

    adding direct_producer

---
+++ src/producer/index-direct_producer.js
@@ -0,0 +1,40 @@
+const config = require('../config');
+const logger = config.logger;
+const createProducer = require('./producer');
+
+// Not needed since producer uses web-hook and know nothing about kafka
+const { Kafka } = require("kafkajs");
+const kafka = new Kafka(config.kafka);
+
+const main = async () => {
+  const consumer = await createProducer({ config: config.producer });
+  logger.info('Producer Connected!');
+
+  const shutdown = async () => {
+    await consumer.disconnect();
+  };
+
+  return shutdown;
+};
+
+const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
+
+main()
+  .then(async (shutdown) => {
+    signalTraps.forEach((signal) => {
+      process.on(signal, async () => {
+        logger.info(`Received ${signal} signal. Shutting down.`);
+        try {
+          await shutdown();
+          process.exit(0);
+        } catch (error) {
+          logger.error('Error during shutdown', error);
+          process.exit(1);
+        }
+      });
+    });
+  })
+  .catch((error) => {
+    logger.error('Error during startup', error);
+    process.exit(1);
+  });
