ec38272a5cecf6948996049316ee8ad048795729
Phạm Thanh Danh <47974698+thanhdanh27600@users.noreply.github.com>
2021-11-22 16:42:03 +0700

    Update delay stats

--- src/client/throughput_calc.py
+++ src/client/throughput_calc.py
@@ -5,12 +5,13 @@ from datetime import datetime
 from utils import plot_util
 
 TIME_COLUMNS = ['timestamp_kafka', 'timestamp_db', 'timestamp_producer']
+DELAY_COLUMNS = ['delay_producer_kafka', 'delay_kafka_db', 'delay_producer_db']
+
 
 def calc(data, throughput_interval, runtime_id, output_folder, collection):
+    (time_data, delay_data) = process_data(data)
     print('Calculating throughput for: ', TIME_COLUMNS)
     print('Throughput interval: ', throughput_interval, 'ms')
-    time_data = process_data(data)
-    # print(time_data.head())
 
     for column_name in TIME_COLUMNS:
         column = time_data[column_name]
@@ -18,42 +19,71 @@ def calc(data, throughput_interval, runtime_id, output_folder, collection):
         end = column.max()
 
         # group by output interval segments and calculate throughput
-        column = column.groupby(pd.cut(column, np.arange(start, end, throughput_interval))).count()
+        column = column.groupby(pd.cut(column, np.arange(
+            start, end, throughput_interval))).count()
         throughput = column/(throughput_interval/1000)
+
+        print(throughput[:10])
 
         # print output
         print('<>____________________')
         print(f'Throughput stats for: {column_name}')
         print(throughput.describe())
         # plot
-        plot_util.line_plot(column, f'thoughput_{column_name}',output_folder, runtime_id)
+        plot_util.line_plot(
+            column, f'thoughput_{column_name}', output_folder, runtime_id)
+
+    for column_name_delay in DELAY_COLUMNS:
+        column = delay_data[column_name_delay]
+
+        # print output
+        print('<>____________________')
+        print(f'Delay stats for: {column_name_delay}')
+        print(column.describe())
+
+        plot_util.line_plot(
+            column, f'delay_{column_name_delay}', output_folder, runtime_id)
 
     # count loss
 
-    prod_id_list = [i['_id'] for i in list(collection.aggregate([ {"$group" : {"_id":"$producer_id"}} ]))]
+    prod_id_list = [i['_id'] for i in list(
+        collection.aggregate([{"$group": {"_id": "$producer_id"}}]))]
     print('\n<>______START COUNT LOSS__________')
     print(f'Producer IDs:')
     for index, val in enumerate(prod_id_list):
         print(f'{index+1}. {val}')
     for index, prod_id in enumerate(prod_id_list):
-        dataListOfProd = [i for i in list(collection.find({"producer_id": prod_id}))]
+        dataListOfProd = [i for i in list(
+            collection.find({"producer_id": prod_id}))]
         toTestList = [i['sequence_num'] for i in dataListOfProd]
-        print(f'PRODUCER {index+1}. First sent at {datetime.fromtimestamp(dataListOfProd[0]["timestamp_producer"]/1000)}, loss {countLoss(toTestList)} packet(s)' )
+        print(
+            f'PRODUCER {index+1}. First sent at {datetime.fromtimestamp(dataListOfProd[0]["timestamp_producer"]/1000)}, loss {countLoss(toTestList)} packet(s)')
     print('<>______END COUNT LOSS__________\n')
-
-    
 
 
 def process_data(data):
     time_data = data[TIME_COLUMNS]
     time_data = time_data.astype(np.int64)
+
+    # count delay from time_data
+    delay_data = pd.DataFrame(
+        columns=['delay_producer_kafka', 'delay_kafka_db', 'delay_producer_db'])
+
+    delay_data['delay_producer_kafka'] = time_data['timestamp_kafka'] - \
+        time_data['timestamp_producer']
+    delay_data['delay_kafka_db'] = time_data['timestamp_db'] - \
+        time_data['timestamp_kafka']
+    delay_data['delay_producer_db'] = time_data['timestamp_db'] - \
+        time_data['timestamp_kafka']
+
     start_time = time_data.min().min()
     time_data = time_data - start_time
-    return time_data
+
+    return (time_data, delay_data)
+
 
 def countLoss(seqList):
     seqList.sort()
-    print(f'DATA. {seqList}')
     l = len(seqList)
     i = 0
     numLoss = 0
